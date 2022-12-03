2849ebcc3e4e39aef6f8aad765107761fed1471f
pedro bufulin <pedro.almeida@ufu.br>
2022-12-02 21:04:06 -0300

    refactor: change position of files and add rust

---
+++ year_2022/target/debug/deps/year_2022-581cd46e2addcb62.d
@@ -0,0 +1,5 @@
+/home/bufulin/Desktop/AdventOfCode/year_2022/target/debug/deps/year_2022-581cd46e2addcb62.rmeta: src/main.rs
+
+/home/bufulin/Desktop/AdventOfCode/year_2022/target/debug/deps/year_2022-581cd46e2addcb62.d: src/main.rs
+
+src/main.rs:
