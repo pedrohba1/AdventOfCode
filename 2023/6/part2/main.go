6158a12acdb15e691f609e5a8a17595df77f740d
pedro bufulin <pedro.almeida@ufu.br>
2023-12-06 17:36:32 -0300

    day 6, part 1 and 2 solutions, including big boi

---
+++ 2023/6/part2/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type Race struct {
+	time     int
+	distance int
+}
+
+func main() {
+	var races []Race
+
+	file, err := os.Open("../input.txt")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	intPattern := regexp.MustCompile(`\d+`)
+	var times []string
+	var distances []string
+	for i := 1; scanner.Scan(); i++ {
+		if i == 1 {
+			times = intPattern.FindAllString(scanner.Text(), -1)
+		} else {
+			distances = intPattern.FindAllString(scanner.Text(), -1)
+		}
+	}
+
+	var time string
+	var distance string
+	for i := 0; i < len(distances); i++ {
+		time += times[i]
+		distance += distances[i]
+
+	}
+	itime, _ := strconv.Atoi(time)
+	idistance, _ := strconv.Atoi(distance)
+
+	races = append(races, Race{
+		time:     itime,
+		distance: idistance,
+	})
+
+	result := 1
+	for _, race := range races {
+		waysToWin := 0
+		for holdTime := 0; holdTime < race.time; holdTime++ {
+			speed := holdTime
+			travelTime := race.time - holdTime
+			if speed*travelTime > race.distance {
+				waysToWin++
+			}
+		}
+		result *= waysToWin
+	}
+
+	fmt.Println("Total ways to win all races:", result)
+}
